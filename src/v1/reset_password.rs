7d831f30a97b672a81e5f48d32b802ec633e11d7
barnlore <Lorenz.Barnsteiner@agcocorp.com>
2021-01-17 21:27:22 +0100

    fix suggestions

--- src/v1/reset_password.rs
+++ src/v1/reset_password.rs
@@ -1,7 +1,6 @@
 //! [POST /_synapse/admin/v1/reset_password/:user_id](https://github.com/matrix-org/synapse/blob/master/docs/admin_api/user_admin_api.rst#reset-password)
 
-use ruma::api::ruma_api;
-use ruma::identifiers::UserId;
+use ruma::{api::ruma_api, identifiers::UserId};
 
 ruma_api! {
     metadata: {
@@ -19,7 +18,7 @@ ruma_api! {
         pub user_id: &'a UserId,
 
         /// new password
-        pub new_password: String,
+        pub new_password: &'a str,
 
         /// Logout all devices of the user, so it necessary to login with the new password again.
         /// Defaults to true.
@@ -27,13 +26,20 @@ ruma_api! {
         pub logout_devices: bool,
     }
 
+    #[derive(Default)]
     response: {}
-
 }
 
 impl<'a> Request<'a> {
     /// Creates an `Request` with the given user ID and the new password.
-    pub fn new(user_id: &'a UserId, new_password: String) -> Self {
+    pub fn new(user_id: &'a UserId, new_password: &'a str) -> Self {
         Self { user_id, new_password, logout_devices: true }
+    }
+}
+
+impl Response {
+    /// Creates an empty `Response`.
+    pub fn new() -> Self {
+        Self
     }
 }
