d4ffd79687876b7f6fbdbae79524526f4d3a4076
barnlore <Lorenz.Barnsteiner@agcocorp.com>
2021-01-18 19:59:13 +0100

    rustfmt

--- src/serde/boolean_as_uint.rs
+++ src/serde/boolean_as_uint.rs
@@ -27,10 +27,7 @@ where
     match u8::deserialize(deserializer)? {
         0 => Ok(false),
         1 => Ok(true),
-        other => Err(de::Error::invalid_value(
-            Unexpected::Unsigned(other as u64),
-            &"zero or one",
-        )),
+        other => Err(de::Error::invalid_value(Unexpected::Unsigned(other as u64), &"zero or one")),
     }
 }
 
