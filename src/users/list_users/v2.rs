f8ef964ca3a8d024ad662b036cc5962a749ebf1d
jamtwister <lorenz_barnsteiner@web.de>
2021-01-22 19:22:45 +0100

    Fix suggestions

--- src/users/list_users/v2.rs
+++ src/users/list_users/v2.rs
@@ -16,35 +16,43 @@ ruma_api! {
 
     #[derive(Default)]
     request: {
-        /// Offset in the returned list. Defaults to 0.
-        #[serde(skip_serializing_if = "Option::is_none")]
+        /// Offset in the returned list.
+        ///
+        /// Defaults to 0.
+        #[serde(default, skip_serializing_if = "ruma::serde::is_default")]
         #[ruma_api(query)]
-        pub from: Option<UInt>,
+        pub from: UInt,
 
         /// Maximum amount of users to return. Defaults to 100.
         #[serde(skip_serializing_if = "Option::is_none")]
         #[ruma_api(query)]
         pub limit: Option<UInt>,
 
-        /// user_id is optional and filters to only return users with user IDs that contain this value. This parameter is ignored when using the name parameter.
+        /// user_id is optional and filters to only return users with user IDs that contain this value.
+        ///
+        /// This parameter is ignored when using the name parameter.
         #[serde(skip_serializing_if = "Option::is_none")]
         #[ruma_api(query)]
-        pub user_id: Option<String>,
+        pub user_id: Option<&'a UserId>,
 
         /// name is optional and filters to only return users with user ID localparts or displaynames that contain this value.
         #[serde(skip_serializing_if = "Option::is_none")]
         #[ruma_api(query)]
-        pub name: Option<String>,
+        pub name: Option<&'a str>,
 
-        /// The parameter guests is optional and if false will exclude guest users. Defaults to true to include guest users.
-        #[serde(skip_serializing_if = "Option::is_none")]
+        /// The parameter guests is optional and if false will exclude guest users.
+        ///
+        /// Defaults to true to include guest users.
+        #[serde(default = "ruma::serde::default_true", skip_serializing_if = "ruma::serde::is_true")]
         #[ruma_api(query)]
-        pub guests: Option<bool>,
+        pub guests: bool,
 
-        /// The parameter deactivated is optional and if true will include deactivated users. Defaults to false to exclude deactivated users.
-        #[serde(skip_serializing_if = "Option::is_none")]
+        /// The parameter deactivated is optional and if true will include deactivated users.
+        ///
+        /// Defaults to false to exclude deactivated users.
+        #[serde(default, skip_serializing_if = "ruma::serde::is_default")]
         #[ruma_api(query)]
-        pub deactivated: Option<bool>,
+        pub deactivated: bool,
     }
 
     response: {
@@ -53,8 +61,9 @@ ruma_api! {
 
         /// Token to receive the next `UserMinorDetails` batch.
         ///
-        /// To paginate, check for next_token and if present, call the endpoint again with from set to the value of next_token. This will return a new page.
-        /// If the endpoint does not return a next_token then there are no more users to paginate through.
+        /// To paginate, check for next_token and if present, call the endpoint again with from set
+        /// to the value of next_token. This will return a new page. If the endpoint does not return
+        /// a next_token then there are no more users to paginate through.
         pub next_token: Option<String>,
 
         /// Total amount of users.
